2d91856958e1ce9371c31c33d47eb3de0acd59ff
Greg Baker <ggbaker@sfu.ca>
2015-09-14 11:18:14 -0700

    comment the puppet files

--- puppet/modules/baseconfig/manifests/init.pp
+++ puppet/modules/baseconfig/manifests/init.pp
@@ -1,3 +1,5 @@
+# an example basic configuration: installs some packages and puts
+# a /etc/ntp.conf file in place
 class baseconfig {
   exec { 'apt-get update':
     command => '/usr/bin/apt-get update',
