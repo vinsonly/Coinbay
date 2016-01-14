fb4711ab12b1550cde225166b040c79fcbab3a91
Greg Baker <ggbaker@sfu.ca>
2016-01-13 16:33:14 -0800

    add an apt sources.list file with SFU sources for faster downloading

--- chef/cookbooks/baseconfig/recipes/default.rb
+++ chef/cookbooks/baseconfig/recipes/default.rb
@@ -1,4 +1,7 @@
 # Make sure the Apt package lists are up to date, so we're downloading versions that exist.
+cookbook_file "apt-sources.list" do
+  path "/etc/apt/sources.list"
+end
 execute 'apt_update' do
   command 'apt-get update'
 end
