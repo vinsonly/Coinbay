8e461b0f67d76ed31489f34ab579da593cd09815
vinsonly <vinsonly@live.ca>
2018-07-17 17:36:19 -0700

    ran sequelize init

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.locals.appName = 'Our Application';
 
