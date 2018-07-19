cc6138204a4c0e8fe1b110d5689a7768eb2ce84f
tamkylet <tamkylet@sfu.ca>
2018-07-18 23:32:29 -0700

    react router implementation

---
+++ client/src/Whoops404.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Whoops404 extends Component {
+  render() {
+    return (
+      <h2>Page does not exist.</h2>
+    );
+  }
+}
+
+export default Whoops404;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+import Whoops404 from './Whoops404'
+
+import registerServiceWorker from './registerServiceWorker';
+import { Router, Route, hashHistory } from 'react-router';
+
+ReactDOM.render(
+	<Router history={hashHistory}>
+		<Route path="/" component={App}/>
+		<Route path="*" component={Whoops404}/>
+	</Router>,
+	document.getElementById('root')
+);
+
+
+
 registerServiceWorker();
