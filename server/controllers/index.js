50697460a14ee4a777e8ba17ab6fbbedd634f2a6
jca248 <jca248@sfu.ca>
2018-07-19 22:04:38 -0700

    added posting controller

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,6 +1,8 @@
 const users = require('./users.js');
+const postings = require('./postings.js');
 
 module.exports = {
     users,
+    postings
     // include your own controllers here
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,16 +1,19 @@
 const usersController = require('../controllers').users;
+const postingsController = require('../controllers').postings;
 
 module.exports = (app) => {
     app.get('/api', (req, res) => res.status(200).send({
-        message: 'Welcome to the Contacts API!'  
+        message: 'Welcome to the Contacts API!'
       }));
 
     app.post('/api/user', usersController.create);
 
     app.get('/api/users', usersController.read);
 
+    app.get('/api/postings', postingsController.read);
+    app.post('/api/posting', postingsController.create);
 
-  
+
     // WRITE THE OTHER ENDPOINTS DOWN HERE
 
     // EXAMPLES:
@@ -19,5 +22,5 @@ module.exports = (app) => {
 
     //  //READ
     //  app.get('/api/contacts', contactsController.read);
- 
+
 }
