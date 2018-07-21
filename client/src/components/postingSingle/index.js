4cfeba68445d9f62f15b43eed49aee274ce4a96a
vinsonly <vinsonly@live.ca>
2018-07-20 20:04:58 -0700

    stashing changes

--- client/src/components/postingSingle/index.js
+++ client/src/components/postingSingle/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 
