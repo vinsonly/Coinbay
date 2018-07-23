745223afdb4d6373ba5982ec6cb8874a71b40390
tamkylet <tamkylet@sfu.ca>
2018-07-22 23:14:09 -0700

    categories nav

--- client/src/components/catNavigation/catNavigation.js
+++ client/src/components/catNavigation/catNavigation.js
@@ -6,32 +6,41 @@ import 'mdbreact/dist/css/mdb.css'
 import './catNavigation.css'
 
 class CatNavigation extends Component {
+	componentDidMount() {
+		// click on left most category to observe effect
+		document.getElementById('expand').addEventListener("click", function() {
+			document.querySelector("#tabs").classList.toggle('expand');
+		});
+	}	
     render() {
         return (
-			<Navbar color="blue lighten-5" light id="no-padding">
-			    <NavbarBrand tag="span" className="tabs">
-			    	<button>Category 1</button>
-			    	<button>Category 1</button>
-			    	<button>Category 1</button>
-			    	<button>Category 1</button>
-			    	<button>Category 1</button>
-			    	<button>Category 1</button>
-			    	<button>Category 1</button>
-			    	<button>Category 1</button>
-			    	<button>Category 1</button>
-			    	<button>Category 1</button>
-			    	<button>Category 1</button>
-			    	<button>Category 1</button>
-			    	<button>Category 1</button>
-			    	<button>Category 1</button>
-			    	<button>Category 1</button>
-			    	<button>Category 1</button>
-			    	<button>Category 1</button>
-			    	<button>Category 1</button>
-			    	<button>Category 1</button>
-			    	<button>Category 1</button>
-			    </NavbarBrand>
-			</Navbar>
+        	<div>
+				<Navbar color="blue lighten-5" light id="no-padding">
+				    <NavbarBrand tag="span" id="tabs">
+				    	<button>Category 1</button>
+				    	<button>Category 2</button>
+				    	<button>Category 3</button>
+				    	<button>Category 4</button>
+				    	<button>Category 5</button>
+				    	<button>Category 6</button>
+				    	<button>Category 7</button>
+				    	<button>Category 8</button>
+				    	<button>Category 9</button>
+				    	<button>Category 10</button>
+				    	<button>Category 11</button>
+				    	<button>Category 12</button>
+				    	<button>Category 13</button>
+				    	<button>Category 14</button>
+				    	<button>Category 15</button>
+				    	<button>Category 16</button>
+				    	<button>Category 17</button>
+				    	<button>Category 18</button>
+				    	<button>Category 19</button>
+				    	<button>Category 20</button>
+				    </NavbarBrand>
+				</Navbar>
+				<p id="expand">Press me</p>
+			</div>
 		)
 	}
 }
--- client/src/components/navigation/Navigation.js
+++ client/src/components/navigation/Navigation.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand, NavbarNav, NavbarToggler, Collapse, NavItem, NavLink, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'mdbreact';
 import { BrowserRouter as Router } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.css'
-import 'mdbreact/dist/css/mdb.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'mdbreact/dist/css/mdb.css';
+import './navigation.css';
 
 class Navigation extends Component {
     constructor(props) {
@@ -28,50 +29,39 @@ class Navigation extends Component {
         });
     }
     render() {
-        return (
-            <Router>
-                <Navbar color="indigo" dark expand="md" scrolling>
-                    <NavbarBrand href="/">
-                        <strong>CryptoBay</strong>
-                    </NavbarBrand>
-                    { !this.state.isWideEnough && <NavbarToggler onClick = { this.onClick } />}
-                    <Collapse isOpen={ this.state.collapse } navbar>
-                        <NavbarNav left>
-                          <NavItem active>
-                              <NavLink to="#">Postings</NavLink>
-                          </NavItem>
-                          <NavItem>
-                              <NavLink to="#">Profile</NavLink>
-                          </NavItem>
-                          <NavItem>
-                              <NavLink to="#">About Us</NavLink>
-                          </NavItem>
-                          <NavItem>
-                              <NavLink to="/metacoin">Meta Coin</NavLink>
-                          </NavItem>
-                          <NavItem>
-                              <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-                              <DropdownToggle nav caret>Categories</DropdownToggle>
-                              <DropdownMenu>
-                                  <DropdownItem href="#">Electronics</DropdownItem>
-                                  <DropdownItem href="#">Games</DropdownItem>
-                                  <DropdownItem href="#">Computers</DropdownItem>
-                                  <DropdownItem href="#">Phones</DropdownItem>
-                              </DropdownMenu>
-                              </Dropdown>
-                          </NavItem>
-                        </NavbarNav>
-                        <NavbarNav right>
-                          <NavItem>
-                            <form className="form-inline md-form mt-0">
-                              <input className="form-control mr-sm-2 mb-0 text-white" type="text" placeholder="Search" aria-label="Search"/>
-                            </form>
-                          </NavItem>
-                        </NavbarNav>
-                    </Collapse>
-                </Navbar>
-            </Router>
-        );
+      return (
+        <Router>
+            <Navbar color="indigo" dark expand="md" scrolling>
+                <NavbarBrand href="/">
+                    <strong>CryptoBay</strong>
+                </NavbarBrand>
+                { !this.state.isWideEnough && <NavbarToggler onClick = { this.onClick } />}
+                <Collapse isOpen={ this.state.collapse } navbar>
+                    <NavbarNav left>
+                      <NavItem active>
+                          <NavLink to="#">Postings</NavLink>
+                      </NavItem>
+                      <NavItem>
+                          <NavLink to="#">Profile</NavLink>
+                      </NavItem>
+                      <NavItem>
+                          <NavLink to="#">About Us</NavLink>
+                      </NavItem>
+                      <NavItem>
+                          <NavLink to="/metacoin">Meta Coin</NavLink>
+                      </NavItem>
+                    </NavbarNav>
+                    <NavbarNav right>
+                      <NavItem>
+                        <form className="form-inline md-form mt-0">
+                          <input className="form-control mr-sm-2 mb-0 text-white" type="text" placeholder="Search" aria-label="Search"/>
+                        </form>
+                      </NavItem>
+                    </NavbarNav>
+                </Collapse>
+            </Navbar>
+        </Router>
+      );
     }
 }
 
