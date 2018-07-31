7ef99e504448b60324dda4726dc5a2f71e53fb46
tamkylet <tamkylet@sfu.ca>
2018-07-30 23:15:46 -0700

    date picker library

---
+++ client/src/components/timePicker/timePicker.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { ClipLoader } from 'react-spinners';
+ 
+class TimePicker extends React.Component {
+
+  render() {
+    return (
+      <p>hello</p>
+    </div>
+    )
+  }
+}
+
+export default TimePicker;
